0d3a5810f70b581db87f6ead399ed64ae9cf99d7
sharath-embryoinc <sharath.embryoinc@gmail.com>
2020-06-21 21:14:52 +0530

    first

---
+++ precache-manifest.bdee5dfaffec23c55d2125626739eb61.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fa09fc94257c4abe21eab9b96c6f74c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0361c8fd91839371e521",
+    "url": "/static/css/2.cc4dc41b.chunk.css"
+  },
+  {
+    "revision": "0bacda46f25d22558529",
+    "url": "/static/css/main.55fb60eb.chunk.css"
+  },
+  {
+    "revision": "0361c8fd91839371e521",
+    "url": "/static/js/2.3d906206.chunk.js"
+  },
+  {
+    "revision": "21b4bc9c6a119502f6018ab9d74c36a1",
+    "url": "/static/js/2.3d906206.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0bacda46f25d22558529",
+    "url": "/static/js/main.f36ccda0.chunk.js"
+  },
+  {
+    "revision": "aee4be1528099ee5c6e0",
+    "url": "/static/js/runtime-main.7d849b27.js"
+  }
+]);
